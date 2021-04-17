b3053bd6f14cb52a49814e4b96c7857554c5ee0d
[Brennan Beeler] <[beeler.b@husky.neu.edu]>
2021-04-16 20:53:59 -0400

    Fixed up register a little. Need to connect to database still.

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -1,62 +1,103 @@
-import React from "react"
+import React, {useState} from "react"
 import {Link} from "react-router-dom";
+import logActions from "../../actions/log-actions";
+import {connect} from "react-redux";
 
-export default class Register extends React.Component {
-    render() {
-        return(
-            //TODO: all values need to be validated and actually do something
-            <div className="container">
-                <h1>Sign Up</h1>
-                <div className="form-group row">
-                    <label htmlFor="usernameFld" className="col-sm-2 col-form-label">
-                        UserName
-                    </label>
-                    <div className="col-sm-10">
-                        <input className="form-control"
-                               id="usernameFld"
-                               placeholder="Username"/>
-                    </div>
-                </div>
+const Register = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
-                <div className="form-group row">
-                    <label htmlFor="passwordFld" className="col-sm-2 col-form-label">
-                        Password </label>
-                    <div className="col-sm-10">
-                        <input className="form-control"
-                               id="passwordFld"
-                               placeholder="Password"/>
-                    </div>
-                </div>
-
-                <div className="form-group row">
-                    <label htmlFor="verifyPasswordFld" className="col-sm-2 col-form-label">
-                        Verify Password </label>
-                    <div className="col-sm-10">
-                        <input className="form-control"
-                               id="verifyPasswordFld"
-                               placeholder="Password"/>
-                    </div>
-                </div>
-
-                <div className="form-group row">
-                    <label className="col-sm-2 col-form-label"/>
-                    <div className="col-sm-10">
-                        <Link to="/profile" className="btn btn-success btn-block">
-                            Sign Up
-                        </Link>
-
-                        <div>
-                            <Link to="/home">
-                                Cancel
-                            </Link>
-                            <Link to="/login" className="float-right">
-                                Already have an account?
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        )
+    const handleSignUp = () => {
+        if (username === "") {
+            alert("Please enter a username")
+        }
+        else if (password === "") {
+            alert("Please enter a password")
+        }
+        else if (confirmPassword === "") {
+            alert("Please confirm your password")
+        }
+        else if (password !== confirmPassword) {
+            alert("Your passwords don't match. Please confirm them both")
+        }
+        //TODO: further validation of all password/username
+        else {
+        //    TODO: try to create user on database
+        }
     }
+
+    return(
+        <div className="container">
+            <br/>
+
+            <h1>Sign Up</h1>
+            <br/>
+
+            <div className="form-group row">
+                <label htmlFor="usernameFld" className="col-sm-2 col-form-label">
+                    UserName
+                </label>
+                <div className="col-sm-10">
+                    <input className="form-control"
+                           id="usernameFld"
+                           placeholder="Username" value={username}
+                           onChange={event => setUsername(event.target.value)}/>
+                </div>
+            </div>
+
+            <div className="form-group row">
+                <label htmlFor="passwordFld" className="col-sm-2 col-form-label">
+                    Password </label>
+                <div className="col-sm-10">
+                    <input className="form-control"
+                           id="passwordFld"
+                           type="password"
+                           placeholder="Password" value={password}
+                           onChange={event => setPassword(event.target.value)}/>
+                </div>
+            </div>
+
+            <div className="form-group row">
+                <label htmlFor="confirmPasswordFld" className="col-sm-2 col-form-label">
+                    Confirm Password </label>
+                <div className="col-sm-10">
+                    <input className="form-control"
+                           type="password"
+                           id="confirmPasswordFld"
+                           placeholder="Confirm password" value={confirmPassword}
+                           onChange={event => setConfirmPassword(event.target.value)}/>
+                </div>
+            </div>
+
+            <div className="form-group row">
+                <label className="col-sm-2 col-form-label"/>
+                <div className="col-sm-10">
+                    <button className="btn btn-success btn-block" onClick={handleSignUp}>
+                        Sign Up
+                    </button>
+
+                    <div>
+                        <Link to="/home">
+                            Cancel
+                        </Link>
+                        <Link to="/login" className="float-right">
+                            Already have an account?
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
+
+const stpm = (state) => ({
+    loggedIn: state.loggedIn
+})
+
+const dtpm = (dispatch) => ({
+//    TODO: need to turn into actual login procedure
+    logIn: (username, password) => logActions.logIn(dispatch, username, password)(dispatch)
+})
+
+export default connect(stpm, dtpm)(Register)
